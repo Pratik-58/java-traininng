091c0e2d31cfbbcee18782132183522f1ef6b09e
Pratik <shindepratik582004@gmail.com>
2025-01-22 10:53:20 +0530

    addition of number

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Add{
+    public static void main(String[] args) {
+        System.out.println("taking input from user :");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number : ");
+        int a = sc.nextInt() ;
+        System.out.println("Enter number : ");
+        int b= sc.nextInt();
+        System.out.println(a+b);
+
+        sc.close();
+    }
+}
---
+++ Per.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Per{
+    public static void main(String[] args) {
+        System.out.println("Enter your marks of 5 subjects :");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter marks of subject 1:");
+        int sub1 = sc.nextInt();
+        
+        System.out.println("enter marks of subject 2:");
+        int sub2 = sc.nextInt();
+        
+        System.out.println("enter marks of subject 3:");
+        int sub3 = sc.nextInt();
+        
+        System.out.println("enter marks of subject 4:");
+        int sub4 = sc.nextInt();
+        
+        System.out.println("enter marks of subject 5:");
+        int sub5 = sc.nextInt();
+    
+        float percentage = (sub1+sub2+sub3+sub4+sub5)/5;
+        System.out.println("percentage of student is : "+percentage);  
+    }    
+}
